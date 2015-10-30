69dc46b92a5a1b03eb455bc7292c12de3cfac223
Eric Bratt <eric_bratt@yahoo.com>
2015-10-30 12:42:58 -0500

    added word count for body scripts

---
+++ csc555/run_wc_body.sh
@@ -0,0 +1,5 @@
+time hadoop jar RCWordCount.jar /user/ec2-user/input/combined /user/ec2-user/output/wordcount_body body no
+
+time hadoop jar RCTop10.jar /user/ec2-user/output/wordcount_body /user/ec2-user/output/wordcount_body_top10
+
+hadoop fs -cat /user/ec2-user/output/wordcount_body_top10/part-r-00000 | head -10  # get the first 10 rows in the output
