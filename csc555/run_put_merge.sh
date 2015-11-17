39e080bb78516103221db93256b0f32cd35af894
Eric Bratt <eric_bratt@yahoo.com>
2015-11-17 11:23:05 -0600

    fixed run_put_merge to include external drive

--- csc555/run_put_merge.sh
+++ csc555/run_put_merge.sh
@@ -1,1 +1,1 @@
-time java -jar PutMerge.jar hdfs://172.31.6.18:9000 ~/data /user/ec2-user/input/combined
+time java -jar PutMerge.jar hdfs://172.31.6.18:9000 /drive1/data /user/ec2-user/input/combined
