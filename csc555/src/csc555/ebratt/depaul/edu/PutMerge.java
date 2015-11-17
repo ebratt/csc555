907f6919c5b3ed58b471e7ab19fcaf9a13862931
Eric Bratt <eric_bratt@yahoo.com>
2015-11-17 07:44:26 -0600

    still not working

--- csc555/src/csc555/ebratt/depaul/edu/PutMerge.java
+++ csc555/src/csc555/ebratt/depaul/edu/PutMerge.java
@@ -26,10 +26,8 @@ package csc555.ebratt.depaul.edu;
 
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 
 /**
@@ -53,28 +51,26 @@ public class PutMerge {
 	 */
 	public static void main(String[] args) throws IOException {
 
+		if (args.length != 2) {
+			System.err.println("Usage: PutMerge.jar <in> <out>");
+			System.exit(2);
+		}
+
 		Configuration conf = new Configuration();
 		FileSystem hdfs = FileSystem.get(conf);
-		FileSystem local = FileSystem.getLocal(conf);
+		FileSystem localFS = FileSystem.getLocal(conf);
 
-		Path inputDir = new Path(args[0]);
+		Path localDir = new Path(args[0]);
 		Path hdfsFile = new Path(args[1]);
 
 		try {
-			FileStatus[] inputFiles = local.listStatus(inputDir);
-			FSDataOutputStream out = hdfs.create(hdfsFile);
-
-			for (int i = 0; i < inputFiles.length; i++) {
-				System.out.println(inputFiles[i].getPath().getName());
-				FSDataInputStream in = local.open(inputFiles[i].getPath());
-				byte buffer[] = new byte[256];
-				int bytesRead = 0;
-				while ((bytesRead = in.read(buffer)) > 0) {
-					out.write(buffer, 0, bytesRead);
-				}
-				in.close();
+			if (hdfs.exists(hdfsFile)) {
+				System.out.println("deleting target file: " + hdfsFile.toString());
+				hdfs.delete(hdfsFile, true);
 			}
-			out.close();
+			System.out.println(
+					"copying/merging files from: local:/" + localDir.toString() + " to hdfs:/" + hdfsFile.toString());
+			FileUtil.copyMerge(localFS, localDir, hdfs, hdfsFile, false, conf, null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
