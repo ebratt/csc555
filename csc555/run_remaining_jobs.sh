c37b177d76e803eb17a31ddf08dcf6e6c0672c66
Eric Bratt <eric_bratt@yahoo.com>
2015-11-20 15:41:19 -0600

    updated remaining jobs script

--- csc555/run_remaining_jobs.sh
+++ csc555/run_remaining_jobs.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 echo "#### STARTING GILDED BY SUBREDDIT - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
 ./run_gilded_sorter_combiner_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING GILDED BY AUTHOR - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_gilded_sorter_group_by_author.sh >> run_remaining_jobs.log 2>&1;
 echo "#### STARTGIN GILDED BY SUBREDDIT - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
 ./run_gilded_sorter_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING GILD PERCENT BY AUTHOR - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_gild_percent_combiner_group_by_author.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING GILD PERCENT BY SUBREDDIT - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_gild_percent_combiner_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING GILD PERCENT BY AUTHOR - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_gild_percent_group_by_author.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING GILD PERCENT BY SUBREDDIT - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_gild_percent_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING VOTES BY AUTHOR - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_vote_sorter_combiner_group_by_author.sh >> run_remaining_jobs.log 2>&1;
 echo "#### STARTING VOTES BY SUBREDDIT - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
 ./run_vote_sorter_combiner_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING VOTES BY AUTHOR - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_vote_sorter_group_by_author.sh >> run_remaining_jobs.log 2>&1;
 echo "#### STARTING VOTES BY SUBREDDIT - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
 ./run_vote_sorter_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
 echo "#### STARTING COUNT OF ACRONYMS BY SUBREDDIT - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
@@ -29,8 +15,3 @@ echo "#### STARTING COUNT OF ACRONYMS BY SUBREDDIT - NO COMBINER ####" >> run_re
 ./run_wc_body_acronyms_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
 echo "#### STARTING COUNT OF ACRONYMS ALL - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
 ./run_wc_body_acronyms.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING COUNT OF BODY WORDS BY SUBREDDIT - WITH COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_wc_body_combiner_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-echo "#### STARTING COUNT OF BODY WORDS BY SUBREDDIT - NO COMBINER ####" >> run_remaining_jobs.log 2>&1;
-./run_wc_body_group_by_subreddit.sh >> run_remaining_jobs.log 2>&1;
-
